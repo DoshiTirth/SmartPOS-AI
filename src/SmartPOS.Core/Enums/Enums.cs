602a205c7ed31b452a5283dce20919d3cdd12478
Tirth Doshi <64912865+DoshiTirth@users.noreply.github.com>
2026-04-22 01:39:37 -0400

    feat: add Core enums - UserRole, TransactionStatus, PaymentMethod, CustomerSegment

---
+++ src/SmartPOS.Core/Enums/Enums.cs
@@ -0,0 +1,39 @@
+﻿namespace SmartPOS.Core.Enums
+{
+    public enum UserRole
+    {
+        Admin = 1,
+        Manager = 2,
+        Cashier = 3
+    }
+
+    public enum TransactionStatus
+    {
+        Completed,
+        Voided,
+        Refunded
+    }
+
+    public enum PaymentMethod
+    {
+        Cash,
+        Card,
+        Other
+    }
+
+    public enum CustomerSegment
+    {
+        New,
+        Occasional,
+        Regular,
+        HighValue,
+        AtRisk
+    }
+
+    public enum StockStatus
+    {
+        InStock,
+        LowStock,
+        OutOfStock
+    }
+}
