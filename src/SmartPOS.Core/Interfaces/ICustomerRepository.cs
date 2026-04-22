8acc35b5d3559ec32c1089f5114f41a48bee1433
Tirth Doshi <64912865+DoshiTirth@users.noreply.github.com>
2026-04-22 02:00:23 -0400

    feat: add Core interfaces - IProductRepository, ITransactionRepository, ICustomerRepository, IDashboardRepository, IAuthService

---
+++ src/SmartPOS.Core/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using SmartPOS.Core.DTOs;
+using SmartPOS.Core.Models;
+
+namespace SmartPOS.Core.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<CustomerDto>> GetAllCustomersAsync();
+        Task<CustomerDto?> GetCustomerByIdAsync(int customerId);
+        Task<CustomerDto?> GetCustomerByEmailAsync(string email);
+        Task<Customer> CreateCustomerAsync(CreateCustomerDto dto);
+        Task<bool> UpdateCustomerAsync(int customerId, CreateCustomerDto dto);
+    }
+}
---
+++ src/SmartPOS.Core/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using SmartPOS.Core.DTOs;
+
+namespace SmartPOS.Core.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task<TransactionResultDto> ProcessTransactionAsync(CreateTransactionDto dto);
+        Task<TransactionDetailDto?> GetTransactionDetailAsync(int transactionId);
+        Task<IEnumerable<TransactionResultDto>> GetRecentTransactionsAsync(int count = 20);
+        Task<bool> VoidTransactionAsync(int transactionId, int userId);
+    }
+}
