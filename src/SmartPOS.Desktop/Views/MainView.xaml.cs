931ef32f8447117c150e46f5cbfe8c6387da90f4
Tirth Doshi <64912865+DoshiTirth@users.noreply.github.com>
2026-04-23 21:10:29 -0400

    fix: CartItem implements INotifyPropertyChanged - quantity display now updates in real time

--- src/SmartPOS.Desktop/Views/MainView.xaml.cs
+++ src/SmartPOS.Desktop/Views/MainView.xaml.cs
@@ -1,27 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SmartPOS.Desktop.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SmartPOS.Desktop.Views
 {
-    /// <summary>
-    /// Interaction logic for MainView.xaml
-    /// </summary>
     public partial class MainView : Window
     {
+        private MainViewModel _viewModel;
+
         public MainView()
         {
             InitializeComponent();
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
